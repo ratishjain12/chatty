b62e89a78ad5bdc2a17f1f73feed51984659b0ed
ratishjain <ratishjain6@gmail.com>
2024-03-08 00:18:28 +0530

    sidebar design and layout

---
+++ frontend/src/components/ChatItem.tsx
@@ -0,0 +1,4 @@
+const ChatItem = () => {
+  return <div>ChatItem</div>;
+};
+export default ChatItem;
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,5 +1,49 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import "./styles.css";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  AddCircle,
+  GroupAdd,
+  Nightlight,
+  PersonAdd,
+} from "@mui/icons-material";
+import ChatItem from "./ChatItem";
+
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sb-header">
+        <div>
+          <IconButton>
+            <AccountCircleIcon />
+          </IconButton>
+        </div>
+        <div>
+          <IconButton>
+            <PersonAdd />
+          </IconButton>
+          <IconButton>
+            <GroupAdd />
+          </IconButton>
+          <IconButton>
+            <AddCircle />
+          </IconButton>
+          <IconButton>
+            <Nightlight />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sb-search">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input placeholder="search" className="searchbox" />
+      </div>
+      <div className="sb-conversations">
+        <ChatItem />
+      </div>
+    </div>
+  );
 };
 export default Sidebar;
