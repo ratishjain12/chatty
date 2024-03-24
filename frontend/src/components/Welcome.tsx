7ee3323a80fe56a20e710b4c93243dcdd6a05f5b
ratishjain <ratishjain6@gmail.com>
2024-03-24 20:31:04 +0530

    added redux toolkit light-dark theme toggle

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -1,11 +1,16 @@
+import { RootState } from "../redux/store";
 import "./styles.css";
+import { useSelector } from "react-redux";
 const Welcome = () => {
+  const theme = useSelector((state: RootState) => state.themeReducer.value);
   return (
     <div className="welcome-container">
       <div className="welcome-content">
         <img className="welcome-logo" src="/chat.png" />
-        <p className="welcome-title">Welcome to Chatty!</p>
-        <p className="welcome-slogan">
+        <p className={"welcome-title" + (theme ? "-dark" : "")}>
+          Welcome to Chatty!
+        </p>
+        <p className={"welcome-slogan" + (theme ? "-dark" : "")}>
           View and text directly with the people in the chat room
         </p>
       </div>
